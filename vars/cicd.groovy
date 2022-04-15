9f29f3111a361e3f64543c1978cb4db0a4f6d0e6
krishnain <86010658+krishnain@users.noreply.github.com>
2022-04-15 08:47:41 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -11,6 +11,11 @@ def newDeploy(ip,appname)
   deploy adapters: [tomcat9(credentialsId: '898c13c4-28fd-4283-82d8-c07f78f8a5e8', path: '', url: "${ip}")], contextPath: "${appname}", war: '**/*.war'
 }
 
+def newTest(jobname)
+{
+  sh 'java -jar /home/ubuntu/.jenkins/workspace/${jobname}/testing.jar'
+}
+
 
 
 
